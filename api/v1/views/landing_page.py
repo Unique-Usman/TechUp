466389dfb3718ebf1c225d1ddfe550f97e242841
Henshaw <josephhensh2000@gmail.com>
2024-03-24 08:31:26 -0700

    Add necessary folders and files for api implementation

---
+++ api/v1/views/landing_page.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Deals with the RESTful API action(s) of the landing page
+"""
+from api.v1.views import app_views
+
+
+@app_views.route("/home", strict_slashes=False)
+def landing_page():
+    """
+    Fetches the necessary data to render the landing page
+    """
+    return jsonify({}), 200
