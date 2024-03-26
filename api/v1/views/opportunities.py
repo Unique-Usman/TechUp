405fd309b466ae5e4aed116aa4d37029bee5ba26
Henshaw <josephhensh2000@gmail.com>
2024-03-26 05:04:36 -0700

    Add more opportunities routes

--- api/v1/views/opportunities.py
+++ api/v1/views/opportunities.py
@@ -48,3 +48,64 @@ def create_opportunity():
     storage.new(opportunity)
     storage.save()
     return jsonify(opportunity.to_dict())
+
+
+@app_views.route("/opportunities/<opportunity_id>", strict_slashes=False)
+def get_opportunity(opportunity_id):
+    """
+    Returns the Opportunity with id `opportunity_id`
+    """
+    opportunity = storage.get(Opportunity, opportunity_id)
+    if not opportunity:
+        abort(404)
+    opportunity = opportunity.to_dict()
+    return jsonify(opportunity)
+
+
+@app_views.route("/opportunities/<opportunity_id>",
+                 methods=["PUT"],
+                 strict_slashes=False)
+def update_opportunity(opportunity_id):
+    """
+    Updates an Opportunity object
+    """
+    opportunity = storage.get(Opportunity, opportunity_id)
+    if not opportunity:
+        abort(404)
+    content_type = request.headers.get("Content-Type")
+    if content_type != "application/json":
+        return jsonify({"message": "Not a JSON"}), 400
+    content = request.get_json()
+    for key, value in content.items():
+        if key not in ["id", "created_at", "updated_at", "user_id"]:
+            setattr(opportunity, key, value)
+    storage.save()
+    return jsonify(opportunity.to_dict()), 200
+
+
+@app_views.route("/opportunities/<opportunity_id>",
+                 methods=["DELETE"],
+                 strict_slashes=False)
+def delete_opportunity(opportunity_id):
+    """
+    Deletes the Opportunity with id `opportunity_id`
+    """
+    opportunity = opportunity.get(Opportunity, opportunity_id)
+    if not opportunity:
+        abort(404)
+    storage.delete(opportunity)
+    storage.save()
+    return jsonify({}), 200
+
+
+@app_views.route("/user/<user_id>/opportunities", strict_slashes=False)
+def get_all_opportunities_of_user(user_id):
+    """
+    Returns all the opportunities of a user with id `user_id`
+    """
+    opportunities = storage.__session.query(Opportunity).filter_by(
+                    user_id=user_id).all()
+    user_opportunities = []
+    for opportunity in opportunites:
+        user_opportunities.append(opportunity.to_dict())
+    return jsonify(user_opportunities)
