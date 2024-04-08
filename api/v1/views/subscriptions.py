83c54f0178b01393acc70f3f79313ad7434e9578
josephhensh <josephhensh2000@gmail.com>
2024-04-08 17:11:25 +0100

    Add subscriptions script: still needs testing

---
+++ api/v1/views/subscriptions.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+Handles all the default RESTful API actions of the Opportunity object
+"""
+from api.v1.views import app_views
+from models.user import subscriptions
+from flask import abort, jsonify, request
+from models import storage
+
+
+@app_views.route("/subscriptions", strict_slashes=False)
+def get_subscriptions():
+    """
+    Retrieves all subscriptions from the database and
+    returns a list containing them
+    """
+    all_subscriptions = storage.all(subscriptions)
+    all_subscriptions_list = []
+    for subscription in all_subscriptions.values():
+        all_subscriptions_list.append(subscription.to_dict())
+    return jsonify(all_subscriptions_list)
+
+
+@app_views.route("/subscriptions/<user_id>/<opp_type_id>",
+                 methods=["POST"], strict_slashes=False)
+def create_subscriptions(user_id, opp_type_id):
+    """
+    Creates a new subscription
+    """
+    content_type = request.headers.get("Content-Type")
+    if content_type != "application/json":
+        return jsonify({"message": "Not a JSON"}), 400
+    content = request.get_json()
+    subscription = subscriptions()
+    setattr(subscription, 'user_id', user_id)
+    setattr(subscription, 'opportunity_type_id', opp_type_id)
+    storage.new(subscription)
+    storage.save()
+    return jsonify(subscription.to_dict())
